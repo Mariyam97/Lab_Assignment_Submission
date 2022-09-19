69bfc4b3763d78bca7e7bd6d817c1d735c4c8ee1
Mariyam_Sardar <87272687+Mariyam97@users.noreply.github.com>
2022-09-20 01:48:07 +0530

    Add files via upload

---
+++ LAB_ASSIGNMENT/IT_Admin/src/Application/Driver.java
@@ -0,0 +1,18 @@
+
+
+package Application;
+
+import CredServices.Employee;
+
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Employee em1=new Employee("Mariyam","Sardar"); //Generate an email
+		
+		System.out.println(em1.showinfo()); // display the Credential
+		
+	}
+}
